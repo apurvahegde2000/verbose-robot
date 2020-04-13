fff2d3277291ac48df5673c88785e953476ec8a4
apurvahegde2000 <63050355+apurvahegde2000@users.noreply.github.com>
2020-04-13 12:54:25 +0530

    Create Contiguous-Array.java

---
+++ Contiguous-Array.java
@@ -0,0 +1,32 @@
+/**
+Given a binary array, find the maximum length of a contiguous subarray with equal number of 0 and 1.
+
+Example 1:
+Input: [0,1]
+Output: 2
+Explanation: [0, 1] is the longest contiguous subarray with equal number of 0 and 1.
+Example 2:
+Input: [0,1,0]
+Output: 2
+Explanation: [0, 1] (or [1, 0]) is a longest contiguous subarray with equal number of 0 and 1.
+Note: The length of the given binary array will not exceed 50,000.
+*/
+public class Solution {
+
+    public int findMaxLength(int[] nums) {
+        int[] arr = new int[2 * nums.length + 1];
+        Arrays.fill(arr, -2);
+        arr[nums.length] = -1;
+        int maxlen = 0, count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            count = count + (nums[i] == 0 ? -1 : 1);
+            if (arr[count + nums.length] >= -1) {
+                maxlen = Math.max(maxlen, i - arr[count + nums.length]);
+            } else {
+                arr[count + nums.length] = i;
+            }
+
+        }
+        return maxlen;
+    }
+}
