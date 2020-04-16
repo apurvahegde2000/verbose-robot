51ccd63c7fffa6be962baa5ceb87d386d06fb3c2
apurvahegde2000 <63050355+apurvahegde2000@users.noreply.github.com>
2020-04-16 14:17:27 +0530

    Create Valid-Parenthesis-String.java

---
+++ Valid-Parenthesis-String.java
@@ -0,0 +1,45 @@
+/**
+Given a string containing only three types of characters: '(', ')' and '*', write a function to check whether this string is valid. We define the validity of a string by these rules:
+
+Any left parenthesis '(' must have a corresponding right parenthesis ')'.
+Any right parenthesis ')' must have a corresponding left parenthesis '('.
+Left parenthesis '(' must go before the corresponding right parenthesis ')'.
+'*' could be treated as a single right parenthesis ')' or a single left parenthesis '(' or an empty string.
+An empty string is also valid.
+Example 1:
+Input: "()"
+Output: True
+Example 2:
+Input: "(*)"
+Output: True
+Example 3:
+Input: "(*))"
+Output: True
+Note:
+1.The string size will be in the range [1, 100].
+
+*/
+class Solution {
+   public boolean checkValidString(String s) {  
+    int leftBalance = 0;
+    for (int i=0; i<s.length(); i++) {
+      if ((s.charAt(i) == '(') || (s.charAt(i) == '*'))
+        leftBalance++;        
+      else
+        leftBalance--;
+        
+      if (leftBalance<0) return false;
+    }
+    if (leftBalance == 0) return true;            
+    int rightBalance = 0;
+    for (int i=s.length()-1; i>=0; i--) {
+      if ((s.charAt(i) == ')') || (s.charAt(i) == '*'))
+        rightBalance++;
+      else
+        rightBalance--;
+      
+      if (rightBalance<0) return false;
+    }
+    return true;
+  }
+}
